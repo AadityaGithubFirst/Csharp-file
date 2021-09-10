49e2b753b6ea0b4550542d5470af5cec2b91363f
Aaditya Hari Nair <aadityaharinair03@gmail.com>
2021-09-10 11:30:42 +0530

    Hi there this is my first repository

---
+++ Creatin an array/Creatin an array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Creatin_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("How many numbers do you want to write");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int [] Numbers =  new int[n];
+            
+            for(int i = 0; i<n; i++)
+            {
+                Console.WriteLine("Enter the " + i + " number");
+                Numbers [i]= Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (var number in Numbers)
+            {
+                Console.Write(number + "\t");
+            }
+        }
+    }
+}
